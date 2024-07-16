5d1ddff3c4251fd1f8c8f04a77cf632571c6d11f
MuzaffarMax <149945517+MuzaffarMax@users.noreply.github.com>
2024-07-17 00:51:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-
-/*
-
-document.getElementById("myH1").textContent ="Hello";
-
- var variable or let variable = document.getElementById("name of variable from ID in html")
-const decrasebtn = document.getElementById("decrasebtn");
-const resetbtn = document.getElementById("resetbtn");
-const increasebtn = document.getElementById("increasebtn");
-//constant variable| doc..gEBI is main part for variable for changing
-
-
- creating variable for counting
-let count = 0 ;
-
- creating function on click what it will do 
-increasebtn.onclick = function(){
-    count ++;
-    countlabel.textContent = count;
-}
-decreasebtn.onclick = function(){
-    count --;
-    countlabel.textContent = count;
-}
-resetbtn.onclick = function(){
-    count = 0;
-    countlabel.textContent = count;
-}
-    */
-
 let text = document.getElementById("parallaxtext")
 let leaf = document.getElementById("leaf")
 let hill1 = document.getElementById("hill1")
