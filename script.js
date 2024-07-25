9338304e3e0cf61900558638048f3cdf49fe6b33
MuzaffarMax <149945517+MuzaffarMax@users.noreply.github.com>
2024-07-25 07:29:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -12,4 +12,36 @@ window.addEventListener('scroll',()=> {
     hill5.style.marginLeft = scrolling * 1.5 + 'px'
     hill4.style.marginLeft = scrolling * -1.5 + 'px'
     tree.style.marginLeft = scrolling * -0.5 + 'px'
+
 })
+
+const decrasebtn = document.getElementById("decrasebtn");
+const resetbtn = document.getElementById("resetbtn");
+const increasebtn = document.getElementById("increasebtn");
+
+
+let count = 0 ;
+
+increasebtn.onclick = function(){
+    count ++;
+    countlabel.textContent = count;
+}
+decreasebtn.onclick = function(){
+    count --;
+    countlabel.textContent = count;
+}
+resetbtn.onclick = function(){
+    count = 0;
+    countlabel.textContent = count;
+}
+
+let roll = document.getElementById("rollbtn");
+const mylabel = document.getElementById("mylabel");
+let min = 1;
+let max = 100;
+
+roll.onclick = function (){
+    roll = Math.floor(Math.random()*max) + min;
+    mylabel.textContent = roll;
+
+}
